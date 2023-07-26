db0d690f969dbc28dbf2026e934e7a924be1b10b
Petr Sedlacek <petr@sedlacek.biz>
2023-07-26 12:31:49 +0200

    [Keyboard] Add 42. Keebs version of Southpaw75

---
+++ keyboards/42keebs/southpaw75/keymaps/vial/config.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8A, 0x9E, 0x58, 0xA2, 0xF1, 0x65, 0xD0, 0x06}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 6 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// Configuration to reduce progmem/flash use
+// #define DYNAMIC_KEYMAP_LAYER_COUNT 2
+// #define VIAL_COMBO_ENTRIES 4
+// #define VIAL_KEY_OVERRIDE_ENTRIES 4
+
+// Extra configuration for the Frood - double tap to reset indicator LED
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED D5
