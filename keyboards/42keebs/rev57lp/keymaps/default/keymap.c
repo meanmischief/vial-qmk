8008fef0b6536155e9b0bf923578453a299600fd
Petr Sedlacek <petr@sedlacek.biz>
2023-10-24 11:45:59 +0200

    42. Keebs Rev57LP: fix keycode alias, add progmem/EEPROM saving config

--- keyboards/42keebs/rev57lp/keymaps/default/keymap.c
+++ keyboards/42keebs/rev57lp/keymaps/default/keymap.c
@@ -26,6 +26,9 @@ enum layer_names {
 #define RAISE  MO(_RAISE)
 #define ADJUST MO(_ADJUST)
 
+#define NUHS KC_NUHS
+#define NUBS KC_NUBS
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_ergo5x12(
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,      KC_5,                        KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
