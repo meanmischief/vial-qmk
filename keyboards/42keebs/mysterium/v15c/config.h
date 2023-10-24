39b2f6cf90879b86ebca29442b342b2904c08090
Petr Sedlacek <petr@sedlacek.biz>
2023-10-24 11:44:51 +0200

    Update 42. Keebs version of Mysterium

--- keyboards/42keebs/mysterium/v15c/config.h
+++ keyboards/42keebs/mysterium/v15c/config.h
@@ -15,9 +15,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-
-#define ENCODERS_PAD_A { C0 }
-#define ENCODERS_PAD_B { C1 }
-#define ENCODER_DIRECTION_FLIP
-#define ENCODER_RESOLUTION 4
-#define TAP_CODE_DELAY 10
